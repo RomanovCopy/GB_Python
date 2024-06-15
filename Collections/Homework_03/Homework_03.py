5258737b8733572d693a98efd642eb8a935c7957
RomanovCopy <RomanovCopy@gmail.com>
2024-06-15 10:43:03 +0300

    CollectionsEnd

--- Collections/Homework_03/Homework_03.py
+++ Collections/Homework_03/Homework_03.py
@@ -5,8 +5,18 @@
 # На входе:
 
 
-# lst = [1, 1, 2, 2, 3, 3]
+lst = [1, 2, 3, 4, 5, 4, 2]
 # На выходе:
 
 
 # [1, 2, 3]
+
+result=set()
+lst.sort()
+length=len(lst)
+if length>0:
+    for i in range(length-1):
+        if lst[i]==lst[i+1]:
+            result.add(lst[i])
+
+print(list(result))
