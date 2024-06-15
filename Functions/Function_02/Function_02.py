9c192ab9a39a08c6e0eab3b54bf2559cb557499b
RomanovCopy <RomanovCopy@gmail.com>
2024-06-15 22:00:28 +0300

    fix1506242200

---
+++ Functions/Function_02/Function_02.py
@@ -0,0 +1,10 @@
+﻿#  Напишите функцию, которая принимает строку текста.
+# ✔ Сформируйте список с уникальными кодами Unicode каждого
+# символа введённой строки отсортированный по убыванию.
+
+def my_func(line: str):
+    unicodes = sorted([ord(line[i]) for i in range(len(line))], reverse=True)
+    return unicodes
+
+
+print(my_func("Romanov"))
--- Functions/Functions_01/Functions_01.py
+++ Functions/Functions_01/Functions_01.py
@@ -1,1 +1,18 @@
+﻿# Напишите функцию, которая принимает строку текста.
+# Вывести функцией каждое слово с новой строки.
+# ✔ Строки нумеруются начиная с единицы.
+# ✔ Слова выводятся отсортированными согласно кодировки Unicode.
+# ✔ Текст выравнивается по правому краю так, чтобы у самого
+# длинного слова был один пробел между ним и номером строки.
 
+import array
+
+
+def my_func(line: str):
+    array = sorted(line.split())
+    my_list = [(i+1, array[i]) for i in range(len(array))]
+    max_length = max(len(s) for _, s in my_list)
+    [print(f"{t[0]} {t[1].rjust(max_length)}") for t in my_list]
+
+
+my_func("Romanov Sergey Mihailovich 1971 2024 ")
