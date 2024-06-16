5f0b982e297138267d68218d9299177cd37693da
RomanovCopy <RomanovCopy@gmail.com>
2024-06-16 19:39:45 +0300

    fix1606241940

---
+++ Functions/Function_05/Function_05.py
@@ -0,0 +1,16 @@
+﻿# Функция принимает на вход три списка одинаковой длины:
+# ✔ имена str,
+# ✔ ставка int,
+# ✔ премия str с указанием процентов вида «10.25%».
+# ✔ Вернуть словарь с именем в качестве ключа и суммой
+# премии в качестве значения.
+# ✔ Сумма рассчитывается как ставка умноженная на процент премии.
+
+def my_func(names, salary_rates, awards):
+    my_dict={}
+    for name, salary_rate, award in zip(names,salary_rates,awards):
+        my_dict[name]=salary_rate+(salary_rate/100*award)
+    return my_dict
+
+print(my_func(["User1", "User2", "User3"],[1500, 1300, 2000],[10.15, 15.0, 12.7]))
+    
---
+++ Functions/Function_06/Function_06.py
@@ -0,0 +1,23 @@
+﻿# Функция получает на вход список чисел и два индекса.
+# ✔ Вернуть сумму чисел между между переданными индексами.
+# ✔ Если индекс выходит за пределы списка, сумма считается
+# до конца и/или начала списка.
+
+from operator import index
+
+
+def my_func(numbers, index_1: int, index_2: int):
+    index_min = min([index_1, index_2])
+    index_max = max([index_1, index_2])
+    length = len(numbers)
+    if length > 2:
+        if index_min < 0 or index_min > length:
+            index_min = 0
+        if index_max < 0 or index_max > length:
+            index_max = len(numbers)
+        return sum(numbers[index_min+1:index_max])
+    else:
+        return 0
+
+
+print(my_func([1, 2, 3, 4, 5, 6, 7, 8], 2, 10))
