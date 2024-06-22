ce35992f86c8450fd1e936ccd4c787dc6a4468d9
RomanovCopy <RomanovCopy@gmail.com>
2024-06-22 10:46:39 +0300

    End

--- Iterators_and_generators/001/_001.py
+++ Iterators_and_generators/001/_001.py
@@ -1,2 +1,28 @@
-﻿a, b, *c = ("roma", "sergey", "mihail", "vladimir", "max", "marat")
-print(f"{a=} {b=} {c=}")
+﻿# data = {"один": 1, "два": 2, "три": 3}
+# x = iter(data.items())
+# print(x)
+# y = next(x)
+# print(y)
+# z = next(iter(y))
+# print(z)
+
+# a = range(0, 10, 2)
+# print(f'{a=}, {type(a)=}, {a.__sizeof__()=}, {len(a)}')
+# b = range(-1_000_000, 1_000_000, 2)
+# print(f'{b=}, {type(b)=}, {b.__sizeof__()=}, {len(b)}')
+
+
+
+# x = [1, 1, 2, 3, 5, 8, 13]
+# y = [1, 2, 6, 24, 120, 720]
+# print(f'{len(x)=}\t{len(y)=}')
+# mult = (i + j for i in x if i % 2 != 0 for j in y if j != 1)
+# res = list(mult)
+# print(f'{len(res)=}\n{res}')
+
+
+data = {2, 4, 4, 6, 8, 10, 7}
+res1 = {None: item for item in data if item > 4}
+res2 = (item for item in data if item > 4)
+res3 = [[item] for item in data if item > 4]
+print(res1, res2, res3)
