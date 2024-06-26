579db311e0853e5e73c6d4e441590bacba256488
Сергей Романов <RomanovCopy@gmail.com>
2024-06-26 15:06:06 +0300

    fix2606241505

--- Files_and_File_System/Lesson_01/Lesson_01.py
+++ Files_and_File_System/Lesson_01/Lesson_01.py
@@ -108,3 +108,110 @@ with open('text_data.txt', 'r', encoding='utf-8') as f:
 # возвращается часть строки заданного размера. При этом следующий вызов метода
 # вернёт продолжение строки снова фиксированного размера или остаток до конца,
 # если она короче.
+
+with open('text_data.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        print(line, end='')        
+        
+
+# Файл построчно попадает в переменную line. А для того чтобы избавиться от пустых
+# строк отключили перенос строки в функции print.
+#  Важно! Символ переноса строки сохранился в конце каждой строки.
+# Если вам необходимо обработать строку без переносов, можно использовать
+# срезы line[:-1] или метод замены line.replace('\n', '')
+
+
+SIZE = 100
+with open('text_data.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        print(line[:-1])
+        print(line.replace('\n', ''))     
+    
+        
+# ➢ w — создаём новый пустой файл для записи. Если файл существует,
+# открываем его для записи и удаляем данные, которые в нём хранились.
+# ➢ x — создаём новый пустой файл для записи. Если файл существует, вызываем
+# ошибку.
+# ➢ a — открываем существующий файл для записи в конец, добавления данных.
+# Если файл не существует, создаём новый файл и записываем в него.
+
+# Метод write принимает на вход строку или набор байт в зависимости от того как вы
+# открыли файл. После записи метод возвращает количество записанной
+# информации.
+text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    res = f.write(text)
+    print(f'{res = }\n{len(text) = }')
+    
+
+# Метод не добавляет символ перехода на новую строку. Если произвести несколько
+# записей, они “склеиваются” в файле.
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing\
+elit.',
+'Consequatur debitis explicabo laboriosam sint suscipit\
+temporibus veniam?',
+'Accusantium alias amet fugit iste neque non odit quia\
+saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        res = f.write(line)
+        print(f'{res = }\n{len(line) = }')
+        
+
+# Если каждая строка должна сохранятся в файле с новой строки, необходимо
+# самостоятельно добавить символ переноса - \n
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing\
+elit.',
+'Consequatur debitis explicabo laboriosam sint suscipit\
+temporibus veniam?',
+'Accusantium alias amet fugit iste neque non odit quia\
+saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        res = f.write(f'{line}\n')
+        print(f'{res = }\n{len(line) = }')
+        
+
+
+# В отличии от write этот метод ничего не возвращает.
+        
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing\
+elit.',
+'Consequatur debitis explicabo laboriosam sint suscipit\
+temporibus veniam?',
+'Accusantium alias amet fugit iste neque non odit quia\
+saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    f.writelines('\n'.join(text))
+    
+# Для того чтобы каждый элемент списка text сохранялся в файле с новой строки
+# воспользовались строковым методом join. writelines не добавляет переноса между
+# элементами последовательности
+
+
+# Функция print по умолчанию выводит информацию в поток вывода. Обычно это
+# консоль. Но можно явно передать файловый объект для печати в файл. Для этого
+# надо воспользоваться ключевым параметром file.
+    
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing\
+elit.',
+'Consequatur debitis explicabo laboriosam sint suscipit\
+temporibus veniam?',
+'Accusantium alias amet fugit iste neque non odit quia\
+saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        print(line, file=f)
+        
+# В отличии от методов записи в файл, функция print добавляет перенос строки.
+# Кроме того ничто не мешает явно изменить параметр end функции.
+        
+text = ['Lorem ipsum dolor sit amet, consectetur adipisicing\
+elit.',
+'Consequatur debitis explicabo laboriosam sint suscipit\
+temporibus veniam?',
+'Accusantium alias amet fugit iste neque non odit quia\
+saepe totam velit?', ]
+with open('new_data.txt', 'a', encoding='utf-8') as f:
+    for line in text:
+        print(line, end='***\n##', file=f)    
